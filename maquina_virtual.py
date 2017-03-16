c365acfc4397ce0150b9a9a28d6bae633bcc5fbb
Rodrigo Rufino Ribeiro <rodrigo.srs.br@gmail.com>
2017-03-16 09:39:11 -0300

    Implementação da função ADD

--- maquina_virtual.py
+++ maquina_virtual.py
@@ -1,3 +1,4 @@
+#coding: utf-8
 """
 Maquina virtual
 EC 208
@@ -7,61 +8,74 @@ PC = 0
 AC = 0
 instr = 0
 intr_type = 0
-data_loc = 0
-data = 0
+data_loc = [0, 0, 0]
+data = [0, 0, 0]
 run_bit = True
+starting_adress = 0
 
 #vetores utilizados como exemplo
-memory = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+memory = ["11110100001000"]
+values = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 instr = ["00000000000000", "00000000000001", "00000000000010", "00000000000011"]
-# 00 adicao, 01 multiplicacao, 10 mod, 11 potencia
+# 00 adicao, 01 multiplicacao, 10 store, 11 load
 #4 bits dado A, 4 bits dado B, 4 bits local onde resultado e armazenado, 2 bits instrucao
 
-# Function definition is here
 def get_instr_type(addr):
-    if addr[13:14] == "00":
+    print(addr[12:] + " 24")
+    if addr[12:] == "00":
+        print("ADD")
         return "ADD"
-    elif addr[13:14] == "01":
+    elif addr[12:] == "01":
         return "MUL"
-    elif addr[13:14] == "10":
-        return "MOD"
-    elif addr[13:14] == "11":
-        return "POT"
+    elif addr[12:] == "10":
+        return "LOAD"
+    elif addr[12:] == "11":
+        return "STORE"
     else:
         return "Instrucao nao encontrada"
 
 def find_data(instr, type):
-    A = instr[0:3]
-    B = instr[5:8]
-    result = instr[9:12]
-    return [A, B, result]
+    A = int(instr[0:4], 2)
+    print(A)
+    B = int(instr[4:8], 2)
+    print(B)
+    C = int(instr[8:11], 2)
+    return [A, B, C]
 
 def execute(type, data):
     if type == "ADD":
+        print("43")
         data[2] = data[0] + data[1]
+        print("ADD: %d = %d + % d" % (data[2],data[1],data[0]))
         return data
     elif type == "MUL":
         data[2] = data[0] * data[1]
         return data
-    elif type == "MOD":
+    elif type == "LOAD":
         data[2] = data[0] % data[1]
         return data
-    elif type == "POT":
+    elif type == "STORE":
         data[2] = data[0] ** data[1]
         return data
     else:
         return "Instrucao nao encontrada"
 
-def interpret(memory, starting_adress):
-    PC = starting_adress
-    while run_bit:
-        instr = memory[PC]
-        PC = PC + 1
-        instr_type = get_instr_type(instr)
-        data_loc = find_data(instr, instr_type)
-        if data_loc >= 0:
-            data = memory[data_loc]
-        execute (instr_type, data)
-    return
+#def interpret(memory, starting_adress):
+PC = starting_adress
+while run_bit:
+    instr = memory[0]
+    PC = PC + 1
+    print("63")
+    instr_type = get_instr_type(instr)
+    data_loc = find_data(instr, instr_type)
+    execute (instr_type, data_loc)
+    print(values)
+    if PC>=len(memory):
+        run_bit = False
+            
+#    return
 
-
+#salvar C no vetor values
+#implementar outras instruções
+#testar mais de uma instrução
+#arquivo
